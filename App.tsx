6e407ba4b0d892d0f9b08c1b94c98b4aa382ea41
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-11-12 17:18:40 +0100

    Fichero Styles.ts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Image, ScrollView } from 'react-native';
+import { View, Image, ScrollView } from 'react-native';
 import { Movie } from './src/Movie';
 import { fetchMovies } from './src/fetchMovies';
+import { Styles } from './src/Styles';
 
-const url_base = "https://api.themoviedb.org/3/movie";
-const key = "c76ed6d50b96d2bfc0920abaeade0be3";
 const routes = {
   now_playing: "/now_playing",
   popular: "/popular",
@@ -23,10 +22,10 @@ export default function App() {
 
   return (
     <View>
-      <ScrollView horizontal={true} style={styles.container2}>
+      <ScrollView horizontal={true} style={Styles.type.container2}>
         {movies.map((item) => (
           <Image
-            style={styles.image}
+            style={Styles.type.image}
             key={item.id.toString()}
             source={{
               uri: `https://image.tmdb.org/t/p/original${item.poster}`,
@@ -38,25 +37,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  movieTitle: {
-    fontSize: 18,
-    marginVertical: 5,
-    color: '#333',
-  },
-  container2: {
-    height: 400,
-  },
-  image: {
-    width: 270,
-    marginTop: 20,
-    margin: 3,
-  },
-});
