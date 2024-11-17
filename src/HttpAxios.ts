1901bff778b78f1379b519e98581f9126d4c1398
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-11-17 18:31:51 +0100

    He cambiado varios detalles

--- src/HttpAxios.ts
+++ src/HttpAxios.ts
@@ -7,8 +7,7 @@ import { movieMapper } from "./MovieMapper";
 export class HttpAxios extends Http {
 
     async getFilms(route: string): Promise<Movie[]> {
-        //const response = await axios.get<MoviesResponse>(`${this.url_base}${route}?api_key=${this.key}`);
-        const response = await axios.get<MoviesResponse>(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${this.page}&api_key=${this.key}`);
+        const response = await axios.get<MoviesResponse>(`${this.url_base}${route}?language=en-US&page=${this.page}&api_key=${this.key}`);
         const dataMovies = response.data.results.map((item: Result) => movieMapper(item));
         return dataMovies;
     }
--- src/fetchMovies.ts
+++ src/fetchMovies.ts
@@ -3,11 +3,6 @@ import { Movie } from "./Movie";
 
 const url_base = "https://api.themoviedb.org/3/movie";
 const key = "c76ed6d50b96d2bfc0920abaeade0be3";
-let page = 1;
-
-export function increasePage() {
-  page++;
-}
 
 export async function fetchMovies(route: string, page: number): Promise<Movie[]> {
   const httpAxios = new HttpAxios({ url_base: url_base, key: key, page: page });
