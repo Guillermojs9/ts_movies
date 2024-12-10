cd9933b7a0b71a8c9de98694dbdd5f3e55fc421a
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-10 18:02:10 +0100

    Tab Navigator

--- App.tsx
+++ App.tsx
@@ -1,18 +1,36 @@
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen } from './src/screens/HomeScreen';
-
-const Stack = createStackNavigator()
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
-
+  const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            switch (route.name) {
+              case "Home":
+                if (focused) return (<Ionicons name="film" size={size} color={color} />)
+                else return (<Ionicons name="film-outline" size={size} color={color} />)
+              case "Settings":
+                if (focused) return (<Ionicons name="build" size={size} color={color} />)
+                else return (<Ionicons name="build-outline" size={size} color={color} />)
+            }
+            return (<Ionicons name="bulb-outline" size={size} color={color} />)
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={HomeScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
 
 
